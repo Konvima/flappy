d0bc4a85098a019bd35e5ccf57391d8ab101f526
MartinKonvicka <martin.konvicka@uhk.cz>
2016-12-08 18:40:56 +0100

    Add BonusTile implementation

--- src/cz/uhk/pro2/flappy/game/tiles/EmptyTile.java
+++ src/cz/uhk/pro2/flappy/game/tiles/EmptyTile.java
@@ -2,7 +2,7 @@ package cz.uhk.pro2.flappy.game.tiles;
 
 import java.awt.Image;
 
-public class EmptyTile extends AbstractWallTile{
+public class EmptyTile extends AbstractTile{
 	
 	public EmptyTile(Image image) {
 		super(image);
--- src/cz/uhk/pro2/flappy/game/tiles/WallTile.java
+++ src/cz/uhk/pro2/flappy/game/tiles/WallTile.java
@@ -2,7 +2,7 @@ package cz.uhk.pro2.flappy.game.tiles;
 
 import java.awt.Image;
 
-public class WallTile extends AbstractWallTile{
+public class WallTile extends AbstractTile{
 	
 	public WallTile(Image image) {
 		super(image);
